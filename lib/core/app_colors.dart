4049b83d98fb5105958ff0435651973f07c3f359
judeadaiy-dev <judeadaiy@gmail.com>
2026-06-04 23:35:47 +0300

    Create app_colors.dart

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF5B8A8A); // اللون الأخضر الأساسي
+  static const Color darkGreen = Color(0xFF3E5C5C); // لون النصوص الغامقة
+  static const Color bgLight = Color(0xFFE8EFF1); // الخلفية الفاتحة
+  static const Color footerBg = Color(0xFF3E5C5C); // خلفية الفوتر
+  static const Color glass = Colors.white; // لون الزجاج
+}
