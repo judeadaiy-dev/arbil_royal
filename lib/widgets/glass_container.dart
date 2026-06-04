7d811893de5c27926a27e5379de4f29aebc9002c
judeadaiy-dev <judeadaiy@gmail.com>
2026-06-04 23:37:14 +0300

    Create glass_container.dart

---
+++ lib/widgets/glass_container.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class GlassContainer extends StatelessWidget {
+  final Widget child;
+  final double borderRadius;
+  final EdgeInsets? padding;
+
+  const GlassContainer({
+    super.key,
+    required this.child,
+    this.borderRadius = 40,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          padding: padding,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(borderRadius),
+            border: Border.all(color: Colors.white.withOpacity(0.4), width: 1.5),
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
