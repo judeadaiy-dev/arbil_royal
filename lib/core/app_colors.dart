6d9ca9c633ab91f993d066fea69b0be5f1605c47
judeadaiy-dev <judeadaiy@gmail.com>
2026-06-06 13:02:08 +0300

    Add dark matte green color constant
    Added a new color constant for dark matte green.

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   // الألوان الأساسية
   static const Color tealGreen = Color(0xFF1E8C7D);
   static const Color darkOliveGrey = Color(0xFF4E4E3F);
+  static const Color darkMatteGreen = Color(0xFF2C5F5D); // ضفناه
   
   // ألوان التدرج للخلفية
   static const Color skyBlueTop = Color(0xFFCDE7F5);
@@ -18,19 +19,14 @@ class AppColors {
   static const Color greyLight = Color(0xFF9E9E9E);
   static const Color greyDark = Color(0xFF616161);
   
-  // ألوان الحالة - ضرورية للأخطاء والنجاح
+  // ألوان الحالة
   static const Color successGreen = Color(0xFF4CAF50);
   static const Color errorRed = Color(0xFFE53935);
   static const Color warningOrange = Color(0xFFFF9800);
   static const Color infoBlue = Color(0xFF2196F3);
   
-  // ألوان إضافية للتصميم
+  // ألوان إضافية
   static const Color primaryLight = Color(0xFF4DB6AC);
   static const Color primaryDark = Color(0xFF00695C);
   static const Color accentGold = Color(0xFFFFD700);
-  
-  // ألوان شفافة
-  static Color tealGreenWithOpacity(double opacity) => tealGreen.withOpacity(opacity);
-  static Color blackWithOpacity(double opacity) => Colors.black.withOpacity(opacity);
-  static Color whiteWithOpacity(double opacity) => Colors.white.withOpacity(opacity);
 }
